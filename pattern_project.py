5ef3d64a077821ec5ef3a2495cf749b306a2601b
Henrique Jensen <henriquejensen@hotmail.com>
2014-03-20 18:04:59 -0300

    Primeiro commit do projeto

---
+++ pattern_project.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from pattern.web import Twitter
+from pattern.db import Database, SQLITE
+from pattern.db import pd
+from pattern.db import field, pk, INTEGER, UNIQUE, STRING
+from sqlite3 import IntegrityError
+
+team = ['#galo', '#Galo', '#Atletico-MG', '#atletico mineiro']
+
+twitter = Twitter()
+db = Database(pd('tweets.db'))
+
+if not "tweets" in db:	
+	db.create("tweets", fields = (pk(), field('code', INTEGER, UNIQUE), field('text', STRING(140))))
+
+#query in Twitter
+for hashtag in team:
+	for tweet in twitter.search(hashtag):
+		try:
+			db.tweets.append(code = tweet.id, text = tweet.text)
+		except IntegrityError:
+			pass
+
+#Separate tweets in database
+for data in db.tweets.filter():
+	print data[2]
+	print '-'*30
+
